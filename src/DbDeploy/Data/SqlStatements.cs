e350208a89dbe08b6baf4c1470fc94ace8e319d7
Adam Beseler <adam.beseler@gmail.com>
2024-12-21 19:21:26 -0600

    removed schema from sql

--- src/DbDeploy/Data/SqlStatements.cs
+++ src/DbDeploy/Data/SqlStatements.cs
@@ -53,7 +53,7 @@ internal static class SqlStatements
     private static class Postgres
     {
         public const string EnsureMigrationTablesExist = """
-            CREATE TABLE IF NOT EXISTS public.__migration_lock (
+            CREATE TABLE IF NOT EXISTS __migration_lock (
                 deployment_id INT GENERATED ALWAYS AS IDENTITY,
                 started_on TIMESTAMP NOT NULL,
                 finished_on TIMESTAMP NULL,
@@ -62,7 +62,7 @@ internal static class SqlStatements
 
             CREATE INDEX IF NOT EXISTS ix__migration_lock__finished_on ON public.__migration_lock (finished_on);
                         
-            CREATE TABLE IF NOT EXISTS public.__migration_history (
+            CREATE TABLE IF NOT EXISTS __migration_history (
                 id INT GENERATED ALWAYS AS IDENTITY,
                 file_name VARCHAR(500) NOT NULL,
                 title VARCHAR(250) NOT NULL,
@@ -76,30 +76,30 @@ internal static class SqlStatements
             """;
 
         public const string AcquireLock = """
-            INSERT INTO public.__migration_lock (started_on)
+            INSERT INTO __migration_lock (started_on)
             SELECT NOW() AT TIME ZONE 'utc'
-            WHERE NOT EXISTS (SELECT 1 FROM public.__migration_lock WHERE finished_on IS NULL)
+            WHERE NOT EXISTS (SELECT 1 FROM __migration_lock WHERE finished_on IS NULL)
             RETURNING deployment_id, started_on, finished_on;
             """;
 
         public const string ReleaseLock = """
-            UPDATE public.__migration_lock
+            UPDATE __migration_lock
             SET finished_on = NOW() AT TIME ZONE 'utc'
             WHERE deployment_id = @DeploymentId;
             """;
 
         public const string GetAllMigrationHistories = """
             SELECT id, file_name, title, executed_on, executed_sequence, hash, deployment_id
-            FROM public.__migration_history;
+            FROM __migration_history;
             """;
 
         public const string InsertMigrationHistory = """
-            INSERT INTO public.__migration_history (file_name, title, executed_on, executed_sequence, hash, deployment_id)
+            INSERT INTO __migration_history (file_name, title, executed_on, executed_sequence, hash, deployment_id)
             VALUES (@FileName, @Title, @ExecutedOn, @ExecutedSequence, @Hash, @DeploymentId);
             """;
 
         public const string UpdateMigrationHistory = """
-            UPDATE public.__migration_history
+            UPDATE __migration_history
             SET executed_on = @ExecutedOn, executed_sequence = @ExecutedSequence, hash = @Hash, deployment_id = @DeploymentId
             WHERE id = @Id;
             """;
@@ -108,8 +108,8 @@ internal static class SqlStatements
     private static class MsSql
     {
         public const string EnsureMigrationTablesExist = """
-            IF OBJECT_ID(N'[dbo].[__migration_lock]', N'U') IS NULL
-            CREATE TABLE [dbo].[__migration_lock] (
+            IF OBJECT_ID(N'[__migration_lock]', N'U') IS NULL
+            CREATE TABLE [__migration_lock] (
                 deployment_id INT NOT NULL IDENTITY(1,1),
                 started_on DATETIMEOFFSET NOT NULL,
                 finished_on DATETIMEOFFSET NULL,
@@ -117,8 +117,8 @@ internal static class SqlStatements
                 INDEX ix__migration_lock__finished_on NONCLUSTERED (finished_on)
             );
 
-            IF OBJECT_ID(N'[dbo].[__migration_history]', N'U') IS NULL
-            CREATE TABLE [dbo].[__migration_history] (
+            IF OBJECT_ID(N'[__migration_history]', N'U') IS NULL
+            CREATE TABLE [__migration_history] (
                 id INT NOT NULL IDENTITY(1,1),
                 file_name VARCHAR(500) NOT NULL,
                 title VARCHAR(250) NOT NULL,
@@ -132,30 +132,30 @@ internal static class SqlStatements
             """;
 
         public const string AcquireLock = """
-            INSERT INTO [dbo].[__migration_lock] ([started_on])
+            INSERT INTO [__migration_lock] ([started_on])
             OUTPUT inserted.deployment_id, inserted.started_on, inserted.finished_on
             SELECT GETUTCDATE()
-            WHERE NOT EXISTS (SELECT * FROM [dbo].[__migration_lock] WHERE finished_on IS NULL);
+            WHERE NOT EXISTS (SELECT * FROM [__migration_lock] WHERE finished_on IS NULL);
             """;
 
         public const string ReleaseLock = """
-            UPDATE [dbo].[__migration_lock]
+            UPDATE [__migration_lock]
             SET [finished_on] = GETUTCDATE()
             WHERE [deployment_id] = @DeploymentId;
             """;
 
         public const string GetAllMigrationHistories = """
             SELECT [id], [file_name], [title], [executed_on], [executed_sequence], [hash], [deployment_id]
-            FROM [dbo].[__migration_history];
+            FROM [__migration_history];
             """;
 
         public const string InsertMigrationHistory = """
-            INSERT INTO [dbo].[__migration_history] ([file_name], [title], [executed_on], [executed_sequence], [hash], [deployment_id])
+            INSERT INTO [__migration_history] ([file_name], [title], [executed_on], [executed_sequence], [hash], [deployment_id])
             VALUES (@FileName, @Title, @ExecutedOn, @ExecutedSequence, @Hash, @DeploymentId);
             """;
 
         public const string UpdateMigrationHistory = """
-            UPDATE [dbo].[__migration_history]
+            UPDATE [__migration_history]
             SET [executed_on] = @ExecutedOn, [executed_sequence] = @ExecutedSequence, [hash] = @Hash, [deployment_id] = @DeploymentId
             WHERE [id] = @Id;
             """;
