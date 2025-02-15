86b8a40ce554e35a963e349b0a326a72aa72e412
Adam Beseler <adam.beseler@gmail.com>
2025-02-15 00:03:46 -0600

    update otel protocol

--- src/DbDeploy/Program.cs
+++ src/DbDeploy/Program.cs
@@ -14,11 +14,7 @@ builder.Logging.AddSerilog(new LoggerConfiguration()
     .WriteTo.OpenTelemetry(options =>
     {
         options.Endpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
-        options.Protocol = builder.Configuration["OTEL_EXPORTER_OTLP_PROTOCOL"] switch
-        {
-            "grpc" => OtlpProtocol.Grpc,
-            _ => OtlpProtocol.HttpProtobuf
-        };
+        options.Protocol = OtlpProtocol.HttpProtobuf;
         var headers = builder.Configuration["OTEL_EXPORTER_OTLP_HEADERS"]?.Split(',') ?? [];
         foreach (var header in headers)
         {
